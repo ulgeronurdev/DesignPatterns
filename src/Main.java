a630e3810b7fdb3835adcbf7ea4d37c237e81ec3
example <ulgeronur@outlook.com>
2023-12-21 17:34:10 +0300

    desing pattern example

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        LampContext context = new LampContext();
+        System.out.println("Lambanın başlangıç durumu : " + context.getLampState().getClass().getName());
+
+        System.out.println();
+        context.onClose();
+        context.onOpen();
+
+        System.out.println("*************************");
+
+        System.out.println("Lambanın şu an durumu : " + context.getLampState().getClass().getName());
+        context.onOpen();
+        context.onClose();
+        System.out.println();
+        System.out.println("Lambanın son durumu : " + context.getLampState().getClass().getName());
+    }
+}
